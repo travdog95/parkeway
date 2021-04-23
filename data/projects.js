09a0296b192cb4ba2542b3568b565a4ebd7d1463
= <=>
2021-04-23 17:47:56 -0600

    Initial commit

---
+++ data/projects.js
@@ -0,0 +1,59 @@
+export default [
+    {
+      route: "project1",
+      title: "Test Project 1",
+      imageUrl: `https://unsplash.it/300/300`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore commodi vero, nam deleniti veritatis! Sed eos doloremque illum aspernatur recusandae. Modi nisi quis nemo!",
+      highlights: [
+        "Prices start in the low 300's",
+        "3 Bedroom, 2 1/2 Bathrooms",
+        "Available in the fall 2021"
+      ]
+    },
+    {
+      route: "project2",
+      title: "Test Project 2",
+      imageUrl: `https://unsplash.it/300/300`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore commodi vero, nam deleniti veritatis! Sed eos doloremque illum aspernatur recusandae. Modi nisi quis nemo!",
+      highlights: [
+        "Prices start in the low 300's",
+        "3 Bedroom, 2 1/2 Bathrooms",
+        "Available in the fall 2021"
+      ]
+    },
+    {
+      route: "project3",
+      title: "Test Project 3",
+      imageUrl: `https://unsplash.it/300/300`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore commodi vero, nam deleniti veritatis! Sed eos doloremque illum aspernatur recusandae. Modi nisi quis nemo!",
+      highlights: [
+        "Prices start in the low 300's",
+        "3 Bedroom, 2 1/2 Bathrooms",
+        "Available in the fall 2021"
+      ]
+    },
+    {
+      route: "project4",
+      title: "Test Project 4",
+      imageUrl: `https://unsplash.it/300/300`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore commodi vero, nam deleniti veritatis! Sed eos doloremque illum aspernatur recusandae. Modi nisi quis nemo!",
+      highlights: [
+        "Prices start in the low 300's",
+        "3 Bedroom, 2 1/2 Bathrooms",
+        "Available in the fall 2021"
+      ]
+    },
+    {
+      route: "project5",
+      title: "Test Project 5",
+      imageUrl: `https://unsplash.it/300/300`,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore commodi vero, nam deleniti veritatis! Sed eos doloremque illum aspernatur recusandae. Modi nisi quis nemo!",
+      highlights: [
+        "Prices start in the low 300's",
+        "3 Bedroom, 2 1/2 Bathrooms",
+        "Available in the fall 2021"
+      ]
+    },
+
+
+  ];
---
+++ js/main.js
@@ -0,0 +1,53 @@
+import Projects from "../data/projects.js";
+
+const buildProjectCards = () => {
+  const projectCardsContainer = document.querySelector('.project-cards-container');
+
+  Projects.map(project => {
+    //Create Card
+    const projectCardDiv = document.createElement("div");
+    projectCardDiv.className = "project-card";
+
+    //Create title
+    const titleDiv = document.createElement("div");
+    titleDiv.className = "project-title";
+    titleDiv.innerHTML = project.title;
+
+    projectCardDiv.append(titleDiv);
+
+    //Create description
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.className = "project-description";
+    descriptionDiv.innerHTML = project.description;
+
+    projectCardDiv.append(descriptionDiv);
+
+    //Create highlights
+    const highlightsUl = document.createElement("ul");
+    highlightsUl.className = "project-highlights";
+
+    project.highlights.forEach(highlight => {
+      const highlightLi = document.createElement("li");
+      highlightLi.className = "project-highlight";
+      highlightLi.innerHTML = highlight;
+      highlightsUl.appendChild(highlightLi);
+    });
+
+    projectCardDiv.append(highlightsUl);
+
+    //Create button
+    const projectButton = document.createElement("button");
+    projectButton.className = "button";
+    projectButton.innerHTML = "Request more info";
+    projectButton.addEventListener("click", (e) => {
+      console.log("Clicked", project);
+    });
+
+    projectCardDiv.append(projectButton);
+
+    projectCardsContainer.append(projectCardDiv);
+
+  });
+}
+
+buildProjectCards();
